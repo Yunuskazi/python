04400ddb955ea5f20b627e2abdca8c3fb6d4262e
Yunus <mdyunusrkk1@gmail.com>
2025-02-11 23:47:34 +0530

    len and different methods

---
+++ length.py
@@ -0,0 +1,10 @@
+#. Count the number of lines and number of characters and number of words in a file?
+line = "Hi how are you"
+word = line.split()
+length = len(line)
+number_of_words = len(word)
+
+print(line)
+print(word)
+print(length)
+print(number_of_words)
