3bc72b1914093754aa294661489efc5ea508ad4a
Yunus <mdyunusrkk1@gmail.com>
2025-02-12 00:00:21 +0530

    reading file

--- length.py
+++ length.py
@@ -8,3 +8,23 @@ print(line)
 print(word)
 print(length)
 print(number_of_words)
+
+
+def count_file_contents(filename):
+    f_line = 0
+    f_char = 0
+    f_word = 0
+
+    with open(filename, "r") as file:
+        for line in file:
+            f_line += 1
+            f_char += 1
+            f_word += 1
+
+    print(f"number of line: {f_line}")
+    print(f"number of character: {f_char}")
+    print(f"number of word: {f_word}")
+
+
+
+count_file_contents("sample.txt")    
